9c5e3fda48d0f321c93d0dab1e1a963603738daa
elap1xi <gangsangwon40@gmail.com>
2022-08-24 23:34:13 +0900

    Update mostwind.py

--- mostwind.py
+++ mostwind.py
@@ -4,30 +4,64 @@ import airportsdata as air
 airports = air.load()
 list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 a = [0]
-def fin(icao):
+'''
+def fin(ic):
    try:
-    airports[icao]
+    airports[ic]
     headers = {
       'Authorization': 'KEY'
     }
-    request = requests.get('https://avwx.rest/api/metar/{0}'.format(icao), headers=headers)
+    request = requests.get('https://avwx.rest/api/metar/{0}'.format(ic), headers=headers)
     response = json.loads(request.text)
     scmws = response['wind_speed']
     ws = int(scmws['repr'])  # wind speed
     if(a[0]<ws):
         a.append(ws)
-        print(f"\nAppend : {icao} = {ws}")
+        print(f"\nAppend : {ic} = {ws}")
     else:
-        print(f"\nNot append : {icao}")
+        print(f"\nNot append : {ic}")
    except:
-    print(f"No Station _ {icao}",end="\r")
+    print(f"No Station _ {ic}",end="\r")
 
 def s():
     for i in list:
         for j in list:
             for k in list:
                 for s in list:
-                    icao = i+j+k+s
-                    fin(icao)
+                    ic = i+j+k+s
+                    fin(ic)
 s()
+print(a)
+'''
+def fin():
+ dop = 0
+ for i in list:
+  for j in list:
+    for k in list:
+      for s in list:
+        ic = i+j+k+s
+        try:
+            airports[ic]
+            headers = {
+              'Authorization': 'KEY'
+            }
+            request = requests.get('https://avwx.rest/api/metar/{0}'.format(ic), headers=headers)
+            response = json.loads(request.text)
+            scmws = response['wind_speed']
+            ws = int(scmws['repr'])  # wind speed
+            if(a[0]<ws):
+                a.append(ws)
+                print(f"\nAppend : {ic} = {ws}")
+            else:
+                print(f"\nNot append : {ic}")
+
+        except:
+            # print(f"No Station _ {ic}",end="\r")
+            dop += 1
+            if(dop%500==0):
+                if(dop%1000==0):
+                 print(f"{dop} _ No station, of {ic}")
+                 pass
+                print(f"{dop} _ No Station")
+fin()
 print(a)
